f146d2fb610016ef9ea509b656f327e20dc19c28
e <cowens@my.uri.edu>
2020-08-23 21:32:58 -0400

    First C++ file for next project
    placeholder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# practice
+# PlaceHolder For next Project
+
+Place Holder for next project practings git 
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+int main()
+{
+    int a, b, c;
+    std::cin >> a;
+    std::cin >> b;
+    c = a - b;
+    std::cout << a << " - " << b << " = " << c << "\n";
+    return 0;
+}
